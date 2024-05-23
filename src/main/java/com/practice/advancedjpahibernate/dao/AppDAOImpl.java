d4745fb11a46c260be77d56adf09674415b429b3
Pranjul Singh <singhpranjul001@gmail.com>
2024-05-23 14:37:12 +0530

    Used Join Fetch to get courses and instructor and also Instructor details.

--- src/main/java/com/practice/advancedjpahibernate/dao/AppDAOImpl.java
+++ src/main/java/com/practice/advancedjpahibernate/dao/AppDAOImpl.java
@@ -80,6 +80,7 @@ public class AppDAOImpl implements AppDAO{
         //create a query
         TypedQuery<Instructor> query = entityManager.createQuery("select i from Instructor i "
                                                         + "JOIN FETCH i.courses "
+                                                        + "JOIN FETCH i.instructorDetail "
                                                         + "where i.id = :data", Instructor.class);
 
         query.setParameter("data", theId);
